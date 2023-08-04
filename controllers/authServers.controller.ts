7733b01f9a97ac2d3794ef5663afa1e768cc41a5
biggaji <tobiajibade@yandex.com>
2023-08-04 08:37:55 +0100

    removed expiry time from signing key

--- controllers/authServers.controller.ts
+++ controllers/authServers.controller.ts
@@ -1,15 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import AuthServerService from "../services/authServer.js";
+import { HydratedServer } from "../types/sharedTypes.js";
+import SigningKeyRepository from "../repositories/signingKey.js";
 const authServerService = new AuthServerService();
+const signingKeyRepository = new SigningKeyRepository();
 
 export const handleAuthServerCreation = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const query = req.body;
     const response = await authServerService.createServer(query);
+    const signingKeyData = await signingKeyRepository.retrieveSigningKeyForServer(response._id as unknown as string);
+    const hydratedServer: HydratedServer = {
+      id: response._id,
+      name: response.name,
+      scope: signingKeyData.scope,
+      serverSigningKey: signingKeyData.key
+    };
+
     res.status(201).json({
       success: true,
-      message: "Auth server created",
-      serverId: response._id
+      message: "Auth server created, please keep your signing key secure",
+      server: hydratedServer
     })
   } catch (error) {
     throw error;
